74e20b357dcbaaf789b0205c02bbe1bad30c343a
unknown <botyz1000@gmail.com>
2026-05-19 22:46:00 -0300

    primeiro commit

---
+++ main.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Fabio
+      * Date: 19-05-26
+      * Purpose: Testes automatizados
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBOL-WORKFLOWS.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM COBOL-WORKFLOWS.
