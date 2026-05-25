9d1d38eb2041693874fd7306ed83acee36ba30a1
unknown <botyz1000@gmail.com>
2026-05-25 02:21:46 -0300

    atualizando o dockerfile

--- testes.cbl
+++ testes.cbl
@@ -3,21 +3,32 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01 NUM1        PIC 9(4) VALUE 2.
-       01 NUM2        PIC 9(4) VALUE 3.
-       01 RESULTADO       PIC 9(4).       
+       01 USUARIO         PIC X(6) VALUE "Fabio".
+       
+       01 USUARIO-VALIDADO     PIC X.
+       
+       01 SENHA           PIC X(6) VALUE '123456'.
+       01 SENHA-VALIDADA  PIC X.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-           
-           CALL "SOMA" USING NUM1 NUM2 RESULTADO
-           IF RESULTADO = 5
-               DISPLAY "PASSOU"
-           ELSE
-           DISPLAY "FALHOU"
+
+           CALL 'LOGIN' USING USUARIO USUARIO-VALIDADO
+               IF USUARIO-VALIDADO = "S"
+                   DISPLAY "TESTE USUARIO PASSOU"
+               ELSE
+                   DISPLAY "TESTE USUARIO FALHOU"
+           STOP RUN RETURNING 1
+           END-IF
+
+           CALL 'SENHA' USING SENHA SENHA-VALIDADA
+               IF SENHA-VALIDADA = "S"
+                  DISPLAY "TESTE SENHA PASSOU"
+               ELSE
+                   DISPLAY "TESTE SENHA FALHOU"
            STOP RUN RETURNING 1
            END-IF
 
             STOP RUN.
            
-       END PROGRAM TESTES.
+           END PROGRAM TESTES.
